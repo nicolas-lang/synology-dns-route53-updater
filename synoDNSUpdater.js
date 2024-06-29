d3432341606ebc090fdb3cbc18e40d413ce4979a
Nicolas Lang <git-related@nicolas-lang.de>
2024-06-29 16:52:04 +0200

    Load configuration from config.json to separate implementation and secrets

--- synoDNSUpdater.js
+++ synoDNSUpdater.js
@@ -17,30 +17,37 @@
 /**************************************
  * Start Configuration
  **************************************/
+const fs = require('fs');
+const path = require('path');
+
+// Load configuration from config.json to separate implementation and secrets
+const configPath = path.join(__dirname, 'config.json');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
 // AWS_ACCESS_KEY
 // AWS Access Key for a user with route53:ChangeResourceRecordSets and route53:ListResourceRecordSets on the domain you want to modify
 // Example: ABCABCABCABCABCABC1A
-var AWS_ACCESS_KEY = "EDIT_ME";
+var AWS_ACCESS_KEY = config.AWS_ACCESS_KEY;
 
 // AWS_SECRET_KEY
 // AWS Secret Key for a user with route53:ChangeResourceRecordSets and route53:ListResourceRecordSets on the domain you want to modify
 // Example: ABCD+v+Abc123Abc123Abc123Abc123Abc123Abc
-var AWS_SECRET_KEY = "EDIT_ME";
+var AWS_SECRET_KEY = config.AWS_SECRET_KEY;
 
 // AWS_REGION
 // AWS Region to act in - won't matter for most records, except R53 'latency optimized'
 // Example: us-west-1
-var AWS_REGION = "EDIT_ME";
+var AWS_REGION = config.AWS_REGION;
 
 // ROUTE53_HOSTED_ZONE_ID
 // The Hosted Zone ID in Route 53 for the domain
 // Example: /hostedzone/ABC123ABC123AB
-var ROUTE53_HOSTED_ZONE_ID = "/hostedzone/EDIT_ME";
+var ROUTE53_HOSTED_ZONE_ID = config.ROUTE53_HOSTED_ZONE_ID;
 
 // ROUTE53_DOMAIN
 // The actual name of the domain name to modify, including an ending period
 // Examples: mysite.com. OR me.myshareddomain.com.
-var ROUTE53_DOMAIN = "EDIT_ME";
+var ROUTE53_DOMAIN = config.ROUTE53_DOMAIN;
 
 /**************************************
  * End Configuration
@@ -72,14 +79,14 @@ var route53 = new AWS.Route53();
 
 var getPublicIP = function () {
     return publicIp.v4()
-	    .then(function(ip) {
-	        currentIP = ip;
-	        console.log("Current public IP is " + currentIP);
-    	}).catch(function(err) {
-	        console.log("Unable to get public IP");
-	        console.log(err);
-	        throw err;
-    	});
+        .then(function(ip) {
+            currentIP = ip;
+            console.log("Current public IP is " + currentIP);
+        }).catch(function(err) {
+            console.log("Unable to get public IP");
+            console.log(err);
+            throw err;
+        });
 };
 
 var getLatestRegisteredIPFromRoute53 = function() {
@@ -90,18 +97,18 @@ var getLatestRegisteredIPFromRoute53 = function() {
         "MaxItems": '1'
     };
     return route53.listResourceRecordSets(params).promise()
-    	.then(function(listResults) {
-    		previousIP = listResults.ResourceRecordSets[0].ResourceRecords[0].Value;
+        .then(function(listResults) {
+            previousIP = listResults.ResourceRecordSets[0].ResourceRecords[0].Value;
             console.log("Successfully polled Route 53 and retrieved Registered IP of " + currentIP);
             notifySynologyDSM("Successfully polled Route 53 and retrieved Registered IP of (" + currentIP + ")");
             return previousIP;
-    	})
-    	.catch(function(err) {
+        })
+        .catch(function(err) {
             console.log("Unable to poll from Route 53!");
             notifySynologyDSM("Error polling route 53 : " + err.code + " / " + err.statusCode);
             console.log(err, err.stack);
             throw err;
-    	});
+        });
 };
 
 var updateRoute53WithNewIP = function() {
@@ -126,17 +133,17 @@ var updateRoute53WithNewIP = function() {
         }
     };
     return route53.changeResourceRecordSets(params).promise()
-    	.then(function() {
+        .then(function() {
             console.log("Successfully updated Route 53");
             notifySynologyDSM("New IP(" + currentIP + ") has successfully been submitted to route 53.");
             return true;
-    	})
-    	.catch(function(err) {
+        })
+        .catch(function(err) {
             console.log("Unable to update Route 53 !");
             notifySynologyDSM("Error updating route 53 : " + err.code + " / " + err.statusCode);
             console.log(err, err.stack);
             throw err;
-    	});
+        });
 };
 
 var notifySynologyDSM = function (body) {
@@ -148,15 +155,15 @@ var notifySynologyDSM = function (body) {
 
 var startPoint = function () {
     Promise.all([getPublicIP(), getLatestRegisteredIPFromRoute53()])
-    	.then(function() {
-		    if (currentIP !== previousIP) {
-		        console.log("Current IP " + currentIP + " differs from Registered IP " + previousIP + "; updating...");
-		        return updateRoute53WithNewIP();
-		    } else {
-		        console.log("Current and Registered IP are both " + currentIP + "; exiting...");
-		        return true;
-		    }
-		});
+        .then(function() {
+            if (currentIP !== previousIP) {
+                console.log("Current IP " + currentIP + " differs from Registered IP " + previousIP + "; updating...");
+                return updateRoute53WithNewIP();
+            } else {
+                console.log("Current and Registered IP are both " + currentIP + "; exiting...");
+                return true;
+            }
+        });
 };
 
 startPoint();
